db2f35087c245e188747aea411056b0b1c9dcda7
artur260790 <126571357+artur260790@users.noreply.github.com>
2023-03-15 18:52:03 +0200

    Add files via upload

---
+++ FirstHomework.java
@@ -0,0 +1,21 @@
+public class FirstHomework {
+
+    public static void main(String[] args) {
+
+    int a = 1144;
+    int b = 978;
+    int c = 12;
+
+
+
+
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(Integer.toBinaryString(c));
+        System.out.println(Long.toOctalString(a));
+        System.out.println(Long.toOctalString(b));
+        System.out.println(Long.toOctalString(c));
+
+
+
+    }}
